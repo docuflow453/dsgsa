72b5109b3abb0d6259f9afad8383cd9ac459d7b4
admire <admire@byteorbit.com>
2026-03-20 17:19:47 +0200

    Updates

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/api',
-  landingBackgroundImageUrl: '/images/background/coming-soon-placeholder.svg'
-};
-
--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
-  landingBackgroundImageUrl: '/images/background/coming-soon-placeholder.svg'
-};
-
